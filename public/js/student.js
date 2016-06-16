f9c58e96692d1fee051e3255d400d519662f89b1
Alan <ajeven87@gmail.com>
2016-06-16 15:12:14 -0500

    Change Average_form to jquery && Do first effects exercise

--- public/js/student.js
+++ public/js/student.js
@@ -24,67 +24,67 @@ var student = {
 	}
 };
 //Disable buttons untill Name is saved
-document.getElementById("add-grade").setAttribute("disabled", true);
-document.getElementById("calculate-average").setAttribute("disabled", true);
+$("#add-grade").attr("disabled", true);
+$("#calculate-average").attr("disabled", true);
 
 var canSubmit = false;
 var buttonMod = function (event) {
-	var i = document.getElementById("name");
-	if (i.value.length > 0) {
+	var i = $("#name");
+	if (i.val().length > 0) {
 		canSubmit = true;
 
 	}
 	if (canSubmit) {
-		document.getElementById("add-grade").removeAttribute("disabled");
-		document.getElementById("calculate-average").removeAttribute("disabled");
+		$("#add-grade").removeAttr("disabled");
+		$("#calculate-average").removeAttr("disabled");
 	}
 }
 
-var save = document.getElementById("save-name");
-save.addEventListener("click", buttonMod, false);
+var save = $("#save-name");
+save.on("click", buttonMod);
 		
-// button disableer ^^^^^^^
+// button disabler ^^^^^^^
 //puts grade and subject in table
 var pushGrade = function (){
-	if (document.getElementById("subject").value !== "" && document.getElementById("grade").value !== "" ) {
-		var subjects = document.getElementById("subject").value;
-		var grades = Number(document.getElementById("grade").value);
+	if ($("#subject").val() !== "" && $("#grade").val() !== "" ) {
+		var subjects = $("#subject").val();
+		var grades = parseInt($("#grade").val());
 		student.addSubject(subjects, grades);
 		var tableEntry = '<tr><td>' + subjects + '</td><td>' + grades + '</td></tr>'
-		document.getElementById("grades").innerHTML = tableEntry + document.getElementById("grades").innerHTML
-		document.getElementById("subject").value = "";
-		document.getElementById("grade").value = "";
+		$("#grades").html(tableEntry + $("#grades").html());
+		$("#subject").val("");
+		$("#grade").val("");
 	} 
 	
 }
 //calculates and pushes average to table
 var pushAverage = function (){
-	var studentAverage = document.getElementById("student-average");
+	var studentAverage = $("#student-average");
 
-	studentAverage.innerHTML = student.calculateAverage();
+	studentAverage.html(student.calculateAverage());
 	if (student.isAwesome()){
-		document.getElementById("student-awesome").removeAttribute("class")
+		$("#student-awesome").removeAttr("class")
 	} else {
-		document.getElementById("student-practice").removeAttribute("class")
+		$("#student-practice").removeAttr("class")
 	}
 	
 }
 
 //puts saved name at top of page
 var pushName = function () {
-	if (document.getElementById("name").value !== "") {
-		studentName.push(document.getElementById("name").value);
-		document.getElementById("student-name").innerHTML = document.getElementById("name").value;
+	if ($("#name").val() !== "") {
+		studentName.push($("#name").val());
+		$("#student-name").html($("#name").val());
 	}
 }
 
-var nameTop = document.getElementById("save-name")
-nameTop.addEventListener("click", pushName, false);
-var gradeAdd = document.getElementById("add-grade")
-gradeAdd.addEventListener("click", pushGrade, false)
-var subjectSave = document.getElementById("subject");
-var average = document.getElementById("calculate-average")
-average.addEventListener("click", pushAverage, false)
+var nameTop = $("#save-name")
+nameTop.on("click", pushName);
+var gradeAdd = $("#add-grade")
+gradeAdd.on("click", pushGrade)
+var subjectSave = $("#subject");
+var average = $("#calculate-average")
+average.on("click", pushAverage)
 
 
 
